d297def298803f39fb5c313f2fbc31bad34bc52e
María Marino <mariamarino@Marias-MacBook-Air.local>
2021-11-01 20:32:03 -0300

    Muevo funciones auxiliares a

--- auxiliares.h
+++ auxiliares.h
@@ -3,5 +3,8 @@
 
 #include "definiciones.h"
 
+void ordenarTablaHogares(vector<vector<int>> &th);
+void ordenarTablaIndividuos(eph_i &ti, eph_h &th);
+
 
 #endif //SOLUCION_AUXILIARES_H
--- ejercicios.cpp
+++ ejercicios.cpp
@@ -66,57 +66,6 @@ join_hi generarJoin( eph_h th, eph_i ti ){
 
 // Implementacion Problema 7
 
-// TODO: abstraer la implementación del bubble sort para ser DRY compliant
-
-void ordenarTablaHogares(vector<vector<int>> &th) {
-    // Implementa un bubble sort para este caso específico
-    for (int i = 0; i < th.size(); i++) {
-        for (int j = th.size() - 1; j > i; j--) {
-            if (
-                    th[j][REGION] < th[j - 1][REGION] ||
-                    (
-                            th[j][REGION] == th[j - 1][REGION] &&
-                            th[j][HOGCODUSU] < th[j - 1][HOGCODUSU]
-                    )
-                )
-            {
-                swap(th[j], th[j - 1]);
-            }
-        }
-    }
-}
-
-int indiceEnTablaHogares(int idx, eph_h const &th) {
-    // Linear search
-    // TODO: is binary search over a table sorted by another value doable?
-    int res = 0;
-    for (int i = 0; i < th.size(); i++) {
-        if (th[i][HOGCODUSU] == idx) {
-            res = i;
-        }
-    }
-    return res;
-}
-
-void ordenarTablaIndividuos(eph_i &ti, eph_h &th) {
-    // Bubble sort
-    for (int i = 0; i < ti.size(); i++) {
-        for (int j = ti.size() - 1; j > i; j--) {
-            if (
-                    indiceEnTablaHogares(ti[j][HOGCODUSU], th) < indiceEnTablaHogares(ti[j - 1][HOGCODUSU], th) ||
-                    (
-                            indiceEnTablaHogares(ti[j][HOGCODUSU], th) == indiceEnTablaHogares(ti[j - 1][HOGCODUSU], th) &&
-                            ti[j][COMPONENTE] < ti[j - 1][COMPONENTE]
-                    )
-                )
-            {
-                swap(ti[j], ti[j - 1]);
-            }
-        }
-    }
-
-}
-
 void ordenarRegionYCODUSU (eph_h &th, eph_i &ti) {
 	ordenarTablaHogares(th);
     ordenarTablaIndividuos(ti, th);
