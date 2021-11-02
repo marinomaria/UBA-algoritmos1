0f8706fbeaeb4d16fb6274e0799429445b58f2c0
María Marino <mariamarino@Marias-MacBook-Air.local>
2021-11-01 21:49:12 -0300

    Exercise 3 - First working version - Passing mandatory tests

--- auxiliares.cpp
+++ auxiliares.cpp
@@ -4,3 +4,40 @@
 #include "ejercicios.h"
 
 
+int cantHabitantes (hogar const &h, eph_i const &ti) {
+    int habitantes = 0;
+    for (individuo i : ti) {
+        if (i[INDCODUSU] == h[HOGCODUSU]) {
+            habitantes++;
+        }
+    }
+    return habitantes;
+}
+
+bool esHogarTarget(hogar const &h, int r) {
+    if (h[REGION] == r && h[IV1] == CASA && h[MAS_500] == 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+float cantCasasTargetConHC(eph_h const &th, eph_i const &ti, int r) {
+    float total = 0.0;
+    for (hogar h : th) {
+        if (esHogarTarget(h, r) && cantHabitantes(h, ti) > 3 * h[II2]) {
+            total++;
+        }
+    }
+
+    return total;
+}
+
+float cantCasasTarget(eph_h const &th, int r) {
+    float total = 0.0;
+    for (const hogar &h : th) {
+        if (esHogarTarget(h, r)) {
+            total++;
+        }
+    }
+    return total;
+}
--- auxiliares.h
+++ auxiliares.h
@@ -3,5 +3,7 @@
 
 #include "definiciones.h"
 
+float cantCasasTargetConHC(eph_h const &th, eph_i const &ti, int r);
+float cantCasasTarget(eph_h const &th, int r);
 
 #endif //SOLUCION_AUXILIARES_H
