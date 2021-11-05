d2f2770f20deb4c5659490c536f5209589a48a74
María Marino <mariamarino@Marias-MacBook-Air.local>
2021-11-05 20:14:04 -0300

    Ejercicio 10 terminado - pasa todos los test - 100% cobertura de líneas

--- auxiliares.h
+++ auxiliares.h
@@ -3,5 +3,8 @@
 
 #include "definiciones.h"
 
+int indiceEnTablaHogares(int idx, eph_h const &th);
+void eliminarHogaresSinIndividuos(eph_i &ti, eph_h &th);
+bool cumpleConBusqueda(individuo i, vector<pair<int, dato>> busqueda);
 
 #endif //SOLUCION_AUXILIARES_H
