9df1e68c0de2c397676fb07cd7994eb83a58c266
María Marino <mariamarino@Marias-MacBook-Air.local>
2021-11-06 16:18:10 -0300

    Implemento un counting sort para ordenarTablaHogares - Pasa tests 100% coverage

--- auxiliares.cpp
+++ auxiliares.cpp
@@ -4,22 +4,34 @@
 #include "ejercicios.h"
 
 // Auxiliares Ejercicio 7
-// TODO: abstraer la implementación del bubble sort para ser DRY compliant
-
 void ordenarTablaHogares(vector<vector<int>> &th) {
-    // Bubble sort
-    for (int i = 0; i < th.size(); i++) {
-        for (int j = th.size() - 1; j > i; j--) {
-            if (
-                    th[j][REGION] < th[j - 1][REGION] ||
-                    (
-                            th[j][REGION] == th[j - 1][REGION] &&
-                            th[j][HOGCODUSU] < th[j - 1][HOGCODUSU]
-                    )
-                    )
-            {
-                swap(th[j], th[j - 1]);
-            }
+    // Counting sort + Bubble Sort
+    // th.size() = n; conteo.size() = m
+
+    int max = 44;
+    int min = 1;
+    int rango = max - min + 1;
+
+    vector<int> conteo(rango);
+    eph_h ordenado(th.size());
+    for (hogar &h : th)                                         // n iteraciones
+        conteo[h[REGION] - min]++;
+
+    for (int i = 1; i < conteo.size(); i++)                     // m iteraciones
+        conteo[i] += conteo[i - 1];
+
+    for (int i = th.size() - 1; i >= 0; i--) {                  // n iteraciones
+        ordenado[conteo[th[i][REGION] - min] - 1] = th[i];
+        conteo[th[i][REGION] - min]--;
+    }
+
+    for (int i = 0; i < th.size(); i++) {                       // n iteraciones
+        th[i] = ordenado[i];
+    }
+
+    for (int i = 1; i < th.size(); i++) {                       // n - 1 iteraciones
+        if (th[i][REGION] == th[i - 1][REGION] && th[i][HOGCODUSU] < th[i - 1][HOGCODUSU]) {
+            swap(th[i], th[i - 1]);
         }
     }
 }
